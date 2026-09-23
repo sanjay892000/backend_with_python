acbc39e056bfd987f963af6884a3155da8f4678c
sanjay892000 <sanjay892000@gmail.com>
2026-09-23 23:05:42 +0530

    Add deployment configuration

--- 02_training_studio/config/settings.py
+++ 02_training_studio/config/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 SECRET_KEY = "django-insecure-iwo4xb_3d7w!j8f*@gzps6*=l%nj_@yh26-9odm+1427px&b6("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
